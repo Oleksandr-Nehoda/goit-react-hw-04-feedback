7d242b856327acd6ab00d2ce99191fe93bfa6444
Oleksandr-Nehoda <iamsoul@ukr.net>
2022-10-29 21:52:39 +0300

    Add-statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Statistics } from './Statistics/Statistics';
+
+class App extends Component {
+  state = {
+    good: 1,
+    neutral: 2,
+    bad: 0,
+  };
+
+  onLeaveFeedBack = () => {
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+  };
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    return Math.round((100 / this.countTotalFeedback()) * this.state.good);
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    console.log();
+    return (
+      <div>
+        <section>
+          <h1>Please leave your feedback</h1>
+          <div>
+            <button onClick={this.onLeaveFeedBack}>Good</button>
+            <button>Neutral</button>
+            <button>Bad</button>
+          </div>
+        </section>
+        <section>
+          <h2>Statistics</h2>
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
